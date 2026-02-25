0fa0315e433f12509c3d62501e50111a245de08c
nikitsya <nikitasmiichyk@gmail.com>
2026-02-25 22:52:17 +0000

    create Customer.java

---
+++ src/main/java/com/nikitsya/billing/customer/Customer.java
@@ -0,0 +1,27 @@
+package com.nikitsya.billing.customer;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    protected Customer() {
+
+    }
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
