e0b15e0beb65865453f2a780cec04267dd2b58ae
nikitsya <nikitasmiichyk@gmail.com>
2026-02-25 11:39:12 +0000

    test: skip testcontainers context test when Docker is unavailable

--- src/test/java/com/nikitsya/billing/BillingPlatformApplicationTests.java
+++ src/test/java/com/nikitsya/billing/BillingPlatformApplicationTests.java
@@ -1,12 +1,24 @@
 package com.nikitsya.billing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.DockerClientFactory;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@EnabledIf("isDockerAvailable")
 class BillingPlatformApplicationTests {
+
+	static boolean isDockerAvailable() {
+		try {
+			return DockerClientFactory.instance().isDockerAvailable();
+		}
+		catch (Exception ex) {
+			return false;
+		}
+	}
 
 	@Test
 	void contextLoads() {
